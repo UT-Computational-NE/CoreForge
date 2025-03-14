cd6226bd919fa3a388c1beccff7da77c650ceac5
Cole <cole.gentry@austin.utexas.edu>
2025-03-14 15:49:09 -0500

    Improved the unit test

--- test/unit/test_pincell.py
+++ test/unit/test_pincell.py
@@ -67,11 +67,11 @@ def test_make_openmc_universe(pincell):
     universe = geom_element.make_openmc_universe()
     assert universe.name == "pincell"
     assert len(universe.cells) == 5
-    assert universe.cells[2].fill.name == "Salt"
-    assert universe.cells[3].fill.name == "Graphite"
-    assert universe.cells[4].fill.name == "Salt"
-    assert universe.cells[5].fill.name == "Graphite"
-    assert universe.cells[6].fill.name == "Salt"
+    assert universe.cells[1].fill.name == "Salt"
+    assert universe.cells[2].fill.name == "Graphite"
+    assert universe.cells[3].fill.name == "Salt"
+    assert universe.cells[4].fill.name == "Graphite"
+    assert universe.cells[5].fill.name == "Salt"
 
 def test_pincell_make_mpact_core(pincell):
     geom_element = pincell
@@ -95,127 +95,32 @@ def test_cylindrical_pincell_initialization(cylindrical_pincell):
     assert geom_element.outer_material.name == "Graphite"
     assert_allclose(geom_element.origin, [0., 0.])
 
-def test_cylindrical_pincell_make_mpact_core(cylindrical_pincell):
+def test_cylindrical_pincell_make_mpact_core(cylindrical_pincell, salt, graphite):
 
     geom_element = cylindrical_pincell
-    model  = Model(core = geom_element.make_mpact_core(), states = [], xsec_settings = {}, options = {})
-    output = model.write_to_string(caseid="test", indent=2)
-    expected_output = \
-"""CASEID test
+    core         = geom_element.make_mpact_core()
+    salt         = salt.mpact_material
+    graphite     = graphite.mpact_material
 
-MATERIAL
-  mat 1 9 2.3275 g/cc 900.0 K \\
-    4009 0.00898964129413786
-    9019 0.05218107467967278
-    3006 1.7431446223252428e-06
-    3007 0.02988796955594127
-    92234 4.115361955751947e-07
-    92235 4.604270687345431e-05
-    92236 2.1089771252934387e-07
-    92238 9.891360788333172e-05
-    40090 0.00047974094545786845
-    40091 0.00010461989131267802
-    40092 0.00015991364848595618
-    40094 0.00016205826301375617
-    40096 2.6108350773217338e-05
-  mat 2 0 1.8599999999999999 g/cc 900.0 K \\
-    5010 1.642700833205197e-08
-    5011 6.645396206175213e-08
-    6001 0.0932567267810358
+    assert len(core.materials) == 2
+    assert salt in core.materials
+    assert graphite in core.materials
 
+    assert isclose(core.mod_dim['X'], 8.0)
+    assert isclose(core.mod_dim['Y'], 8.0)
+    assert_allclose(core.mod_dim['Z'], [1.0])
 
-GEOM
-  mod_dim 8.0 8.0 1.0
-
-  pinmesh 1 gcyl 0.5 1.0 1.5 2.0 2.5 3.0 / -4.0 4.0 -4.0 4.0 / 1.0 / 1 1 1 1 1 1 / 8 8 8 8 8 8 8 / 1
-
-  pin 1 1 / 1 1 2 2 1 1 2
-
-  module 1 1 1 1
-    1
-
-  lattice 1 1 1
-    1
-
-  assembly 1
-    1
-
-  core 360
-    1
-
-XSEC
-
-OPTION
-
-"""
-    assert output == expected_output
+    assert len(core.pins)       == 1
+    assert len(core.modules)    == 1
+    assert len(core.lattices)   == 1
+    assert len(core.assemblies) == 1
 
     mpact_build_specs = deepcopy(geom_element.mpact_build_specs)
     mpact_build_specs.divide_into_quadrants = True
     geom_element.mpact_build_specs = mpact_build_specs
+    core = geom_element.make_mpact_core()
 
-    model  = Model(core = geom_element.make_mpact_core(), states = [], xsec_settings = {}, options = {})
-    output = model.write_to_string(caseid="test", indent=2)
-    expected_output = \
-"""CASEID test
-
-MATERIAL
-  mat 1 9 2.3275 g/cc 900.0 K \\
-    4009 0.00898964129413786
-    9019 0.05218107467967278
-    3006 1.7431446223252428e-06
-    3007 0.02988796955594127
-    92234 4.115361955751947e-07
-    92235 4.604270687345431e-05
-    92236 2.1089771252934387e-07
-    92238 9.891360788333172e-05
-    40090 0.00047974094545786845
-    40091 0.00010461989131267802
-    40092 0.00015991364848595618
-    40094 0.00016205826301375617
-    40096 2.6108350773217338e-05
-  mat 2 0 1.8599999999999999 g/cc 900.0 K \\
-    5010 1.642700833205197e-08
-    5011 6.645396206175213e-08
-    6001 0.0932567267810358
-
-
-GEOM
-  mod_dim 4.0 4.0 1.0
-
-  pinmesh 1 gcyl 0.5 1.0 1.5 2.0 2.5 3.0 / -4.0 0.0 0.0 4.0 / 1.0 / 1 1 1 1 1 1 / 8 8 8 8 8 8 8 / 1
-  pinmesh 2 gcyl 0.5 1.0 1.5 2.0 2.5 3.0 / 0.0 4.0 0.0 4.0 / 1.0 / 1 1 1 1 1 1 / 8 8 8 8 8 8 8 / 1
-  pinmesh 3 gcyl 0.5 1.0 1.5 2.0 2.5 3.0 / -4.0 0.0 -4.0 0.0 / 1.0 / 1 1 1 1 1 1 / 8 8 8 8 8 8 8 / 1
-  pinmesh 4 gcyl 0.5 1.0 1.5 2.0 2.5 3.0 / 0.0 4.0 -4.0 0.0 / 1.0 / 1 1 1 1 1 1 / 8 8 8 8 8 8 8 / 1
-
-  pin 1 1 / 1 1 2 2 1 1 2
-  pin 2 2 / 1 1 2 2 1 1 2
-  pin 3 3 / 1 1 2 2 1 1 2
-  pin 4 4 / 1 1 2 2 1 1 2
-
-  module 1 1 1 1
-    1
-  module 2 1 1 1
-    2
-  module 3 1 1 1
-    3
-  module 4 1 1 1
-    4
-
-  lattice 1 2 2
-    1 2
-    3 4
-
-  assembly 1
-    1
-
-  core 360
-    1
-
-XSEC
-
-OPTION
-
-"""
-    assert output == expected_output
-
+    assert isclose(core.mod_dim['X'], 4.0)
+    assert isclose(core.mod_dim['Y'], 4.0)
+    assert core.lattices[0].nx == 2
+    assert core.lattices[0].ny == 2
