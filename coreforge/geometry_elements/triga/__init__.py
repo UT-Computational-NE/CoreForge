c96f89081ad5af8cd1e7f12f3c5ac9d8b2670e57
Cole Gentry <cgentry7@gmail.com>
2025-11-24 11:41:57 -0500

    Added GraphiteElementPincell

--- coreforge/geometry_elements/triga/__init__.py
+++ coreforge/geometry_elements/triga/__init__.py
@@ -1,5 +1,7 @@
 from .fuel_element_pincell import FuelElementPincell
+from .graphite_element_pincell import GraphiteElementPincell
 
 __all__ = [
     "FuelElementPincell",
+    "GraphiteElementPincell",
 ]
